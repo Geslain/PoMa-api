47e1e9161933f0c4005e7ed08b05ffa9b488b78a
Geslain Dahan <geslain.dahan@spaceable.org>
2024-03-29 05:38:45 +0700

    feat(404):  Add interceptor for handling 404 errors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NotFoundInterceptor } from './not-found.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-
+  app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(3000);
 }
 bootstrap();
