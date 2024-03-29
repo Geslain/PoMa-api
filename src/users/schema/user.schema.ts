947dbcd35389df05a3f0802e5a4d11b07c73864d
Geslain Dahan <geslain.dahan@spaceable.org>
2024-03-29 16:50:57 +0700

    fix(user): fix context error for password hashing function

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -42,11 +42,11 @@ UserSchema.pre('save', function (next) {
   if (!this.isModified('password')) return next();
 
   // generate a salt
-  bcrypt.genSalt(10, function (err, salt) {
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) return next(err);
 
     // hash the password using our new salt
-    bcrypt.hash(this.password, salt, function (err, hash) {
+    bcrypt.hash(this.password, salt, (err, hash) => {
       if (err) return next(err);
       // override the cleartext password with the hashed one
       this.password = hash;
